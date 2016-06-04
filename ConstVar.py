4fa5d76266e2331d439b053a0b4a6eebce9e8066
13522955396@163.com <Git5241osc>
2016-06-04 22:20:24 +0800

    init

--- ConstVar.py
+++ ConstVar.py
@@ -8,24 +8,22 @@ import const
 # 而python是解释型语言，得运行时才会抛出异常
 const.STATIC_VAR = 5
 print const.STATIC_VAR
-# const.STATIC_VAR = 6  # 这一行会抛出异常
+#const.STATIC_VAR = 6  # 这一行会抛出异常
 
 # 变量声明与java的区别
 # java中声明变量必须声明变量的类型 如 int i=6;
 i = 6
-print i
+
 # java中变量的类型不能变化【不能把int赋值给String】而python则可以
 i = "一二三"
-print i
+
 # 通过序列解包(sequence unpacking)可以将多个值的序列解开，让后一一放置到变量的序列中。
 # 解包的序列中的元素必须和等号左边变量数量一致。
-values = 1, 2, 3#生成的是一个元组【后面会介绍】
+values = "1", 2, True#生成的是一个元组【后面会介绍】
 
 print values  # output: (1, 2, 3)
 print values[0]
 x, y, z = values  # output: 1 2 3
-print x, y, z
-
 bob_Info = {'name': 'Bob', 'email': 'bob@gmail.com'}
 key, value = bob_Info.popitem()
 print value  # output: Bob
